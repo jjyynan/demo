d2dbe3345705b684029e441e9cefd98717fcc5ed
Administrator <Administrator@ZN-PC-2>
2018-03-21 21:47:34 +0800

    11

--- demo3/src/main/java/com/example/demo/DemoApplication.java
+++ demo3/src/main/java/com/example/demo/DemoApplication.java
@@ -25,9 +25,9 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@RequestMapping(value="/{abc8}",method=RequestMethod.GET)
+	@RequestMapping(value="/{abc9}",method=RequestMethod.GET)
 	@ResponseBody
-	public String addUser(@PathVariable("abc8") Long id) {
+	public String addUser(@PathVariable("abc9") Long id) {
 		RestTemplate restTemplate=new RestTemplate();
 		String url="http://192.168.3.85:8081/user/";
 		UserEntity userEntity=new UserEntity();
